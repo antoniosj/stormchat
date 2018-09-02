fa562b85abe61507575738fa3d695d943fb58e22
Antônio Jr <antonio.silvajr23@gmail.com>
2018-09-02 19:08:30 -0300

    Add broadcast receiver.

--- app/src/main/java/com/antoniosj/stormchat/controller/MainActivity.kt
+++ app/src/main/java/com/antoniosj/stormchat/controller/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.antoniosj.stormchat.controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.antoniosj.stormchat.R
+import com.antoniosj.stormchat.services.AuthService
+import com.antoniosj.stormchat.services.UserDataService
+import com.antoniosj.stormchat.utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +30,19 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(BROADCAST_USER_DATA_CHANGE))
+    }
+
+    private val userDataChangeReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (AuthService.isLoggedIn) {
+                userNameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                loginButtonNavHeader.text = "logout"
+            }
+        }
     }
 
     override fun onBackPressed() {
